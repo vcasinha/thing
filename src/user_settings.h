66475374d96bf6aa83f7cfb326f7274de62ce306
miro <vcasinha@gmail.com>
2018-08-17 12:57:28 +0100

    Added user settings

---
+++ src/user_settings.h
@@ -0,0 +1,14 @@
+#ifndef USER_SETTINGS
+#define USER_SETTINGS
+
+    const char * name = "RObot";
+    unsigned int serial_port_baud_rate = 115200;
+
+    const char * mqtt_hostname = "192.168.0.200";
+    const char * mqtt_username = "mqtt";
+    const char * mqtt_password = "mqtt";
+
+    // const char * mqtt_hostname = "192.168.1.64";
+    // const char * mqtt_username = "mqtt";
+    // const char * mqtt_password = "mqtt";
+#endif
